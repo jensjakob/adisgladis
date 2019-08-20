0d55f3a6961337a9fa4c3e4bebd758bfba8cd49d
jensjakob <jensjakob@users.noreply.github.com>
2019-08-20 14:01:09 +0200

    logo on category page

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+
+add_action( 'woocommerce_archive_description', 'adisgladis_woocommerce_category_image', 0 );
+function adisgladis_woocommerce_category_image() {
+	if ( is_product_category() ){
+		global $wp_query;
+		$cat = $wp_query->get_queried_object();
+		$thumbnail_id = get_woocommerce_term_meta( $cat->term_id, 'thumbnail_id', true );
+		$image = wp_get_attachment_url( $thumbnail_id );
+		?>
+
+		<div class="category-header">
+		<?php if ( $image ) { ?>
+			<?php echo $image ?>
+		<?php } else { ?>
+			<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
+		<?php } ?>
+		</div>
+
+		<?php
+	}
+}
+?>
